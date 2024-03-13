263d5e1bb6ad289dcee9ea14333eeace26f588b7
Jackiets98 <100361056+Jackiets98@users.noreply.github.com>
2024-03-13 13:06:58 +0800

    another trial

---
+++ android/app/src/main/kotlin/com/yta/customer/MainActivity.kt
@@ -0,0 +1,8 @@
+package com.yta.customer
+
+import io.flutter.embedding.android.FlutterFragmentActivity
+import io.flutter.embedding.engine.FlutterEngine
+
+class MainActivity: FlutterFragmentActivity() {
+
+}
